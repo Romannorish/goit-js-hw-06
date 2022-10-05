3ff48619aabcd02748c3df6370e826101a974d95
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2022-10-05 23:57:03 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+const ulEl = document.querySelector("#categories");
+console.log(ulEl);
+
+const liEl = document.querySelectorAll(".item");
+
+console.log(liEl);
+
+liEl.forEach(() => {
+    console.log(liEl);
+});
+
+
+const title = document.querySelectorAll("h2");
+
+console.log(title[2]);
+
+
