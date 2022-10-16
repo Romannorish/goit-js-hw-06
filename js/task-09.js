3201ef1f8084ebd12405ed90c381745ddb421648
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2022-10-16 23:14:57 +0300

    up to hw 6

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const buttonRef = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+console.log(buttonRef);
+console.log(spanEl);
+
+
+buttonRef.addEventListener('click', () => {
+  spanEl.textContent = getRandomHexColor();
+  document.body.setAttribute(
+    'style',
+    'background-color:' + spanEl.textContent
+  )
+});
+
+
+
